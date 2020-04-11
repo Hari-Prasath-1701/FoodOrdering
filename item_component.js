8efd71315d1fe248fe2114e1e973e74bbd58a281
Hari Prasath <43212406+Hari-Prasath-1701@users.noreply.github.com>
2020-04-11 09:08:57 +0530

    Add files via upload

---
+++ item_component.js
@@ -0,0 +1,91 @@
+Vue.component('Chickenbiriyani_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Indian/chickenbiriyani.jpg" style="height: 60px;width: 60px;">      <div class="header" >        ChickenBiriyani  <span style="margin-left: 40px;"></span>    $7</div><div class="meta">110 cal/kg</div></div> </div></div>'
+});
+Vue.component('Dosai_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Indian/dosai.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Dosai  <span style="margin-left: 40px;"></span>    $5</div><div class="meta">30 cal/Dosai</div></div> </div></div>'
+});
+Vue.component('Meals_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Indian/meals.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Meals  <span style="margin-left: 40px;"></span>    $10</div><div class="meta">235 cal/Meal</div></div> </div></div>'
+});
+Vue.component('Naan_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Indian/naan.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Naan  <span style="margin-left: 40px;"></span>    $4</div><div class="meta">40 cal/Naan</div></div> </div></div>'
+});
+Vue.component('Thali_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Indian/thali.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Thali  <span style="margin-left: 40px;"></span>    $6</div><div class="meta">35 cal/Dosai</div></div> </div></div>'
+});
+Vue.component('Dimsum_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Chinese/dimsum.jpg" style="height: 60px;width: 60px;">      <div class="header" >        DimSums  <span style="margin-left: 40px;"></span>    $3</div><div class="meta">12 cal/Dimsum</div></div> </div></div>'
+});
+Vue.component('Springroll_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Chinese/springroll.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Sprinroll  <span style="margin-left: 40px;"></span>    $5</div><div class="meta">70 cal/Roll</div></div> </div></div>'
+});
+Vue.component('Szechawn_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Chinese/szechwan.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Szechawn  <span style="margin-left: 40px;"></span>    $4.5</div><div class="meta">20 cal/Plate</div></div> </div></div>'
+});
+Vue.component('Shitake_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Chinese/shitake.jpg" style="height: 60px;width: 60px;">      <div class="header" >        Shitake  <span style="margin-left: 40px;"></span>    $6</div><div class="meta">60 cal/shitake</div></div> </div></div>'
+});
+Vue.component('Risotto_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Italian/risotto.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Mushroom Risotto  <span style="margin-left: 40px;"></span>    $2.5</div><div class="meta">40 cal/risotto</div></div> </div></div>'
+});
+Vue.component('Tiramisu_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Italian/tiramisu.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Tiramisu - "The Pick-me-up" Cake  <span style="margin-left: 40px;"></span>    $3</div><div class="meta">15 cal/cake</div></div> </div></div>'
+});
+Vue.component('Frocaccia_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Italian/risotto.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Focaccia Bread  <span style="margin-left: 40px;"></span>    $2</div><div class="meta">10 cal/Bread</div></div> </div></div>'
+});
+Vue.component('Bruschetta_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Italian/bruschetta.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Bruschetta  <span style="margin-left: 40px;"></span>    $2.8</div><div class="meta">10 cal/Bread</div></div> </div></div>'
+});
+Vue.component('Caprese_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Italian/caprese.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Caprese Salad with Pesto Sauce  <span style="margin-left: 40px;"></span>    $2.8</div><div class="meta">30 cal/Salad</div></div> </div></div>'
+});
+Vue.component('Grilled_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Greek/grilled.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Grilled meat   <span style="margin-left: 40px;"></span>    $4</div><div class="meta">120 cal/Grill</div></div> </div></div>'
+});
+Vue.component('Olive_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Greek/olive.jpg" style="height: 60px;width: 60px;">      <div class="header" >      Olives and Olive Oil   <span style="margin-left: 40px;"></span>    $3</div><div class="meta">45 cal/Olive</div></div> </div></div>'
+});
+Vue.component('Taramasalata_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Greek/taramasalata.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Taramasalata   <span style="margin-left: 40px;"></span>    $2</div><div class="meta">20 cal/Scoop</div></div> </div></div>'
+});
+Vue.component('Moussaka_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Greek/moussaka.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Moussaka   <span style="margin-left: 40px;"></span>    $5</div><div class="meta">35 cal/Scoop</div></div> </div></div>'
+});
+Vue.component('Khao_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Thai/khao.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Khao Phat or Khao Pad (Fried Rice)   <span style="margin-left: 40px;"></span>    $4</div><div class="meta">40 cal/Bowl</div></div> </div></div>'
+});
+Vue.component('Gang_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Thai/gang.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Gaeng Keow Wan Gai (Green Chicken Curry)   <span style="margin-left: 40px;"></span>    $4</div><div class="meta">35 cal/Bowl</div></div> </div></div>'
+});
+Vue.component('Pad_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Thai/pad.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Pad Thai (Stir-Fried Noodles)   <span style="margin-left: 40px;"></span>    $4</div><div class="meta">20 cal/Bowl</div></div> </div></div>'
+});
+Vue.component('Tom_component',{
+   template : '<div class="ui card">  <div class="card">    <div class="content">    <i class="left floated th list icon" ></i>      <img class=" left floated mini ui image" src="Image/Thai/tom.jpg" style="height: 60px;width: 60px;">      <div class="header" >       Tom Kha Gai (Chicken in Coconut Soup)   <span style="margin-left: 40px;"></span>    $4</div><div class="meta">70 cal/Bowl</div></div> </div></div>'
+});
+
+var vm1 = new Vue({
+   el: '.all'
+});
+var vm1 = new Vue({
+   el: '.india'
+});
+var vm1 = new Vue({
+   el: '.greek'
+});
+var vm1 = new Vue({
+   el: '.china'
+});
+var vm1 = new Vue({
+   el: '.italy'
+});
+var vm1 = new Vue({
+   el: '.thai'
+});
+var vm1 = new Vue({
+   el: '.s1'
+});
+var vm1 = new Vue({
+   el: '.s2'
+});
